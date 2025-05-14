f0862c60028c3d5026ce856d98ab21ffc30d61cb
kmmr119 <ak119.kmmr@gmail.com>
2025-05-14 22:58:10 +0900

    model, historyの保存機能を追加実装

--- src/infer.py
+++ src/infer.py
@@ -4,8 +4,14 @@ import numpy as np
 def inference(model, image, device='cpu'):
     model.eval()
     with torch.no_grad():
+        # imageがNumPy配列の場合: Tensorに変換
         if isinstance(image, np.ndarray):
             image = torch.from_numpy(image.transpose(2, 0, 1)).unsqueeze(0).float()
+
+        # imageが3次元 (C, H, W) の場合、バッチ次元 (N=1) を追加
+        if image.ndim == 3:
+            image = image.unsqueeze(0).float()
+
         image = image.to(device)
         output = model(image)['out']
         pred = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
